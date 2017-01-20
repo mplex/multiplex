4378f2e068d16a07f2e57f0d8698c89ae31d9c27
mplex <multiplex@post.com>
2017-01-20 11:26:14 +0100

    v2.5 beta version

--- R/jnt.R
+++ R/jnt.R
@@ -1,7 +1,7 @@
 jnt <-
-function (xj, prsep) 
+function (xj, sep) 
 {
-    ifelse(missing(prsep) == TRUE, prsep <- ", ", NA)
+    ifelse(missing(sep) == TRUE, sep <- ", ", NA)
     if (isTRUE(length(xj) != 0) == TRUE) {
         if (isTRUE(is.list(xj)) == TRUE) {
             Xj <- list()
@@ -11,10 +11,10 @@ function (xj, prsep)
                 if (isTRUE(length(xj[[i]]) != 0) == TRUE) {
                   tmpj <- as.list(xj[[i]])
                   for (j in 1:length(xj[[i]])) {
-                    ifelse(isTRUE("." %in% strsplit(prsep, "")[[1]]) == 
+                    ifelse(isTRUE("." %in% strsplit(sep, "")[[1]]) == 
                       TRUE, Xj[[i]] <- append(Xj[[i]], tmpj[[j]]), 
                       Xj[[i]] <- append(Xj[[i]], strsplit(tmpj[[j]], 
-                        prsep)[[1]]))
+                        sep)[[1]]))
                   }
                   rm(j, tmpj)
                   Xj[[i]] <- unique(Xj[[i]])
@@ -22,10 +22,10 @@ function (xj, prsep)
                     jt[[i]] <- Xj[[i]]
                   if (length(Xj[[i]]) > 1) 
                     jt[[i]] <- paste(Xj[[i]][1], Xj[[i]][2], 
-                      sep = prsep)
+                      sep = sep)
                   if (length(Xj[[i]]) > 2) {
                     for (j in 3:length(Xj[[i]])) jt[[i]] <- paste(jt[[i]], 
-                      Xj[[i]][j], sep = prsep)
+                      Xj[[i]][j], sep = sep)
                     rm(j)
                   }
                 }
@@ -38,17 +38,17 @@ function (xj, prsep)
         else if (isTRUE(is.vector(xj)) == TRUE) {
             vec <- vector()
             for (i in 1:length(xj)) {
-                vec <- append(vec, strsplit(xj[i], prsep)[[1]])
+                vec <- append(vec, strsplit(xj[i], sep)[[1]])
             }
             rm(i)
             vec <- levels(factor(vec))
             if (length(vec) == 1) 
                 jt <- vec
             if (length(vec) > 1) 
-                jt <- paste(vec[1], vec[2], sep = prsep)
+                jt <- paste(vec[1], vec[2], sep = sep)
             if (length(vec) > 2) {
                 for (i in 3:length(vec)) jt <- paste(jt, vec[i], 
-                  sep = prsep)
+                  sep = sep)
                 rm(i)
             }
         }
