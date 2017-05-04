115394c5770a0a1b8d8363683018a77453fc4c1f
mplex <multiplex@post.com>
2017-05-04 11:37:09 +0200

    v2.6

--- R/write.srt.R
+++ R/write.srt.R
@@ -3,10 +3,10 @@ function (x, file = NULL, sep = "\t", header = TRUE)
 {
     if (isTRUE(is.na(dim(x)[3]) == TRUE) == FALSE) {
         if (isTRUE(is.null(dimnames(x)[[3]])) == TRUE) 
-            dimnames(x)[[3]] <- 1:dim(x)[3]
+            dimnames(x)[[3]] <- seq_len(dim(x)[3])
     }
     if (isTRUE(is.null(dimnames(x)[[1]])) == TRUE) 
-        dimnames(x)[[1]] <- dimnames(x)[[2]] <- 1:dim(x)[1]
+        dimnames(x)[[1]] <- dimnames(x)[[2]] <- seq_len(dim(x)[1])
     if (isTRUE(dim(x)[3] == 1) == TRUE) 
         x <- x[, , 1]
     if (isTRUE(is.na(dim(x)[3]) == TRUE) == TRUE) {
@@ -14,8 +14,8 @@ function (x, file = NULL, sep = "\t", header = TRUE)
             cat(paste("Sender", "Receiver", "Ties", sep = sep), 
                 file = file, sep = "\n", append = TRUE)
         }
-        tmp <- transf(x, lb2lb = TRUE, labels = dimnames(x)[[1]])
-        for (i in 1:length(tmp)) {
+        tmp <- transf(x, type = "tolist", lb2lb = TRUE, lbs = dimnames(x)[[1]])
+        for (i in seq_len(length(tmp))) {
             cat(paste(strsplit(tmp[i], ", ")[[1]][1], strsplit(tmp[i], 
                 ", ")[[1]][2], "1", sep = sep), file = file, 
                 sep = "\n", append = TRUE)
@@ -25,9 +25,10 @@ function (x, file = NULL, sep = "\t", header = TRUE)
     else if (isTRUE(is.na(dim(x)[3]) == TRUE) == FALSE) {
         ts <- rep(0L, dim(x)[3])
         DF <- data.frame(matrix(ncol = (dim(x)[3] + 2L), nrow = 0L))
-        for (k in 1:dim(x)[3]) {
-            tmp <- transf(x[, , k], lb2lb = TRUE, labels = dimnames(x)[[1]])
-            for (i in 1:length(tmp)) {
+        for (k in seq_len(dim(x)[3])) {
+            tmp <- trnf(x[, , k], tolist = TRUE, lb2lb = TRUE, 
+                lbs = dimnames(x)[[1]])
+            for (i in seq_len(length(tmp))) {
                 ts[k] <- 1L
                 DF[(nrow(DF) + 1L), ] <- c(strsplit(tmp[i], ", ")[[1]][1], 
                   strsplit(tmp[i], ", ")[[1]][2], ts)
@@ -39,7 +40,7 @@ function (x, file = NULL, sep = "\t", header = TRUE)
         isol <- dimnames(x)[[1]][which(!(dimnames(x)[[1]] %in% 
             dimnames(rm.isol(x))[[1]]))]
         if (isTRUE(length(isol) > 0L) == TRUE) {
-            for (i in 1:length(isol)) {
+            for (i in seq_len(length(isol))) {
                 DF[(nrow(DF) + 1L), ] <- c(isol[i], isol[i], 
                   ts)
             }
