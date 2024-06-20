22c8ca1cda9bfe916c4307c3148d37bd7b58b025
AOST <AOST@via.dk>
2024-06-20 13:44:14 +0200

    rm deprecated

--- R/is.mc.R
+++
@@ -1,7 +0,0 @@
-is.mc <-
-function (B, C, A, ord = NULL) 
-{
-    return(!(all(dichot(trnf(B, tolist = FALSE, ord = ord) + 
-        trnf(C, tolist = FALSE, ord = ord), c = 2) == trnf(A, 
-        tolist = FALSE, ord = ord))))
-}
